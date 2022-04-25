368b363f16634c0fa89869a8813eb4691056a97e
Said Nizamutdinov <nsrwork@gmail.com>
2022-04-25 19:21:57 +0300

    Update type index

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,45 +1,40 @@
 // Trade.js
 export declare class Trade {
   constructor();
-  getAverageOpenPrice(): string;
-  getAverageClosePrice(): string;
-  getPriceChangeInPercent(): string;
+  getAverageOpenPrice(): any;
+  getAverageClosePrice(): any;
+  getAveragePrice(price: any): any;
+  getPriceChangeInPercent(): any;
   calVolume(): void;
-  addVolume({ quantity, price }: { quantity: any; price: any }): void;
-  getVolume(): string;
+  addVolume({ quantity, price }: {
+    quantity: any;
+    price: any;
+  }): void;
+  getVolume(): any;
   calPositionSize(): void;
   addSizeToPosition(size: any): void;
   subtractSizeFromPosition(size: any): void;
-  getMaxPositionSize(): string;
+  getMaxPositionSize(): any;
   getRestOfPositionSize(): number;
   addPnl(pnl: any): void;
-  getProfit(): string;
-  getFees(): string;
+  getProfit(): any;
+  getFees(): any;
   addFees(fees: any): void;
   getSide(): "LONG" | "SHORT";
-  openNewPosition({
-    symbol,
-    side,
-    openTime,
-    tickSize,
-    pricePrecision,
-  }: {
+  openNewPosition({ symbol, side, openTime }: {
     symbol: any;
     side: any;
     openTime: any;
-    tickSize: any;
-    pricePrecision: any;
   }): void;
   isPositionNotEmpty(): boolean;
-  increasePosition({ price, quantity }: { price: any; quantity: any }): void;
-  closePosition({ closeTime }: { closeTime: any }): void;
-  modifyPosition({
-    side,
-    price,
-    quantity,
-    realizedPnl,
-    fees,
-  }: {
+  increasePosition({ price, quantity }: {
+    price: any;
+    quantity: any;
+  }): void;
+  closePosition({ closeTime }: {
+    closeTime: any;
+  }): void;
+  modifyPosition({ side, price, quantity, realizedPnl, fees }: {
     side: any;
     price: any;
     quantity: any;
@@ -56,13 +51,14 @@ export declare function tradeAggregator(rawTrades: any): any;
 export declare function groupAggregatedTrades(rawData: any): any;
 export declare function groupRawTradesBySymbols(rawData: any): any[][];
 
-// symbolProperties.js
-export declare function getProps(symbolName: any): any;
-
 // utils.js
-export declare function calPriceChangeInPercentage(
-  openPrice: any,
-  closePrice: any
-): number;
+export declare function calPriceChangeInPercentage(openPrice: any, closePrice: any): string;
 export declare function calAveragePrice(list: any): number;
-export declare function calPricePrecision(ticketSize: string): number;
+export declare function getPrecision(price: any): number;
+export declare function currencyNormalizer(money: any, options?: {}): string;
+
+// calculation.js
+export declare function add(...number: any[]): any;
+export declare function subtract(...number: any[]): any;
+export declare function multiply(...number: any[]): any;
+export declare function divide(...number: any[]): any;
